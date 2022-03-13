9e55cd686edb7af0dc184fb2d7c08b90d86240ae
Andrew Lisowski <lisowski54@gmail.com>
2022-03-12 16:28:59 -0800

    init webpack

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
   });
 
   win.loadFile(path.join(__dirname, "index.html"));
